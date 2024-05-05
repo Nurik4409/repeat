2b69b1b08538a98a634254185c04c93da141b010
Nurik4409 <Nurmuhammad2220@icloud.com>
2024-05-04 21:23:00 -0700

    gnfhgfh

--- main.py
+++ main.py
@@ -13,15 +13,15 @@
 # else:
 #     print("false")
 
-tst = 15
-
-if 10 < tst < 20:
-    print("tst больше 10 и меньше 20")
-else:
-    print("tst не удовлетворяет условию")
-
-
-
+# tst = 15
+#
+# if 10 < tst < 20:
+#     print("tst больше 10 и меньше 20")
+# else:
+#     print("tst не удовлетворяет условию")
+a = 'flf'
+b = 'gom'
+print(a*b)
 
 
 
