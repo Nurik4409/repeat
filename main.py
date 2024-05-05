9a97256ce50c51f9e48dcddf816b0262377ee32c
Nurik4409 <Nurmuhammad2220@icloud.com>
2024-05-04 19:54:45 -0700

    class work

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #1. операторы больше и меньше в python
-tst = 3
-tst = int(input("give me number"))
+# tst = 3
+# tst = int(input("give me number"))
+# if tst > 0:
+#     print("true")
+# else:
+#     print("false")
+
+#class work
+tst = 5
 if tst > 0:
     print("true")
 else:
     print("false")
-
-
-
-
 
 
 
